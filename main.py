a307d9363b86a90ea5e0117a6224eda912472c55
Miguel Andres Herize Godoy <miguel.herize@mykukun.com>
2023-03-26 00:48:10 -0400

    change server

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(
     openapi_url="/api/v1/openapi.json",
     docs_url="/docs",
     redoc_url="/redoc",
-    servers=[{"url": "https://migherize.github.io/CredencialSports"}],
+    servers=[{"url": "https://migherize.github.io/CredencialSports/api"}],
 )
 
 
